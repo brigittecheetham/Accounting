2cc3808d9ec5e76d8caabe0055d29285a5ed55de
Brigitte <brigitte.cheetham@gmail.com>
2019-06-08 16:21:37 +0200

    Initialise the project

---
+++ Ledgers.Api/Controllers/GeneralLedgerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ledgers.Api.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Ledgers.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneralLedgerController : ControllerBase
+    {
+        [HttpPost]
+        [Route("postTransaction")]
+        public IActionResult PostTransaction([FromBody]SourceTransactionDto sourceTractionDto)
+        {
+            try
+            {
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ Ledgers.Api/Models/SourceTransaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ledgers.Api.Models
+{
+    public class SourceTransaction
+    {
+    }
+}
