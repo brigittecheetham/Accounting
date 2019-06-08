ebf78bbd96a2379f0958c21b96739bfd0328edf6
Brigitte <brigitte.cheetham@gmail.com>
2019-06-08 16:43:24 +0200

    Add RabbitMQClient class to manage connections to RabbitMQ

---
+++ Ledgers.Api/RabbitMQ/RabbitMQClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ledgers.Api.Models;
+using RabbitMQ.Client;
+
+namespace Ledgers.Api.RabbitMQ
+{
+    public class RabbitMQClient
+    {
+        private IConnection _connection;
+        private IModel _model;
+        private QueueingBasicConsumer _consumer;
+        private string _replyQueueName;
+
+        public void CreateConnection()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _replyQueueName = _model.QueueDeclare("rpc_reply", true, false, false, null);
+            _consumer = new QueueingBasicConsumer(_model);
+            _model.BasicConsume(_replyQueueName, true, _consumer);
+        }
+
+        public void CloseConnection()
+        {
+            _connection.Close();
+        }
+
+        public string PostTransaction(SourceTransaction sourceTransaction)
+        {
+
+            return string.Empty;
+
+        }
+
+    }
+}
